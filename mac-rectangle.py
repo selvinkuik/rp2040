5c2cf761836b8657a69b55d870b9ce79ac95554f
Sel-Vin Kuik <selvinkuik@gmail.com>
2024-04-08 15:55:43 +0100

    feat: rectangle update

--- mac-rectangle.py
+++ mac-rectangle.py
@@ -5,16 +5,16 @@ app = {
     'macros' : [
         # 1st row ----------
         (0x3d0203, '<', [Keycode.CONTROL, Keycode.OPTION, Keycode.LEFT_ARROW]),
-        (0x361503, 'C', [Keycode.CONTROL, Keycode.OPTION, 'C']),
+        (0x361503, 'C', [Keycode.CONTROL, Keycode.OPTION, 'c']),
         (0x361e02, '>', [Keycode.CONTROL, Keycode.OPTION, Keycode.RIGHT_ARROW]),
         # 2nd row ----------
-        (0x3e1602, '< 2/3', [Keycode.CONTROL, Keycode.OPTION, 'E']),
-        (0x000000, '', []),
-        (0x1c2912, '> 2/3', [Keycode.CONTROL, Keycode.OPTION, 'T']),
+        (0x3e1602, '< 2/3', [Keycode.CONTROL, Keycode.OPTION, 'e']),
+        (0x3f2d02, '[C]', [Keycode.CONTROL, Keycode.OPTION, Keycode.COMMAND, 'r', -Keycode.COMMAND, Keycode.COMMAND, Keycode.OPTION, 'c']),
+        (0x1c2912, '> 2/3', [Keycode.CONTROL, Keycode.OPTION, 't']),
         # 3rd row ----------
-        (0x0d221c, '< 1/3', [Keycode.CONTROL, Keycode.OPTION, 'D']),
-        (0x0f1d1c, 'C 1/3', [Keycode.CONTROL, Keycode.OPTION, 'F']),
-        (0x000000, '> 1/3', [Keycode.CONTROL, Keycode.OPTION, 'G']),
+        (0x0d221c, '< 1/3', [Keycode.CONTROL, Keycode.OPTION, 'd']),
+        (0x0f1d1c, 'C 1/3', [Keycode.CONTROL, Keycode.OPTION, 'f']),
+        (0x000000, '> 1/3', [Keycode.CONTROL, Keycode.OPTION, 'g']),
         # 4th row ----------
         (0x000000, '', []),
         (0x000000, '', []),
