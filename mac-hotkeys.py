21eeb54c4d87fc621aedeffc780e6e08621d6012
Sel-Vin Kuik <selvinkuik@gmail.com>
2024-04-15 09:06:05 +0100

    fix: hotkeys

--- mac-hotkeys.py
+++ mac-hotkeys.py
@@ -10,11 +10,11 @@ app = {
         # 2nd row ----------
         (0x3e1602, 'Finder', [Keycode.COMMAND, Keycode.OPTION, '3']),
         (0x3f2d02, 'Fork', [Keycode.COMMAND, Keycode.OPTION, '4']),
-        (0x1c2912, 'Missive', [Keycode.COMMAND, Keycode.OPTION, '5']),
+        (0x1c2912, 'Missive', [Keycode.COMMAND, Keycode.OPTION, '6']),
         # 3rd row ----------
-        (0x0d221c, 'Slack', [Keycode.COMMAND, Keycode.OPTION, '6']),
-        (0x0f1d1c, 'Term.', [Keycode.COMMAND, Keycode.OPTION, '7']),
-        (0x000000, 'VS Code', [Keycode.COMMAND, Keycode.OPTION, '8']),
+        (0x0d221c, 'Slack', [Keycode.COMMAND, Keycode.OPTION, '7']),
+        (0x0f1d1c, 'Term.', [Keycode.COMMAND, Keycode.OPTION, '8']),
+        (0x000000, 'VS Code', [Keycode.COMMAND, Keycode.OPTION, '9']),
         # 4th row ----------
         (0x11171d, '[<Tab]', [Keycode.COMMAND, Keycode.SHIFT, '[']),
         (0x11171d, '[Tab>]', [Keycode.COMMAND, Keycode.SHIFT, ']']),
